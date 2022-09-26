2da96c77e87adcb9ca5541dd8dc635caf7851bfe
Riya505 <riyapk505@gmail.com>
2022-09-26 17:04:48 +0530

    employee added

---
+++ src/Employee/Employees.java
@@ -0,0 +1,36 @@
+package Employee;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Employees {
+    public static void main(String[] args) {
+        System.out.println("employee");
+        int choice;
+        String empName;
+
+        ArrayList<String> empList=new ArrayList<String>();
+
+        while(true){
+            System.out.println("choose option");
+            System.out.println("1. add employee");
+            System.out.println("2. view employee");
+            System.out.println("3. exit");
+            Scanner scanner=new Scanner(System.in);
+            choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("add employee name");
+                    empName=scanner.next();
+                    empList.add(empName);
+                    break;
+                case 2:
+                    System.out.println(empList);
+                    break;
+                case 3:
+                    System.exit(0);
+
+            }
+        }
+    }
+}
