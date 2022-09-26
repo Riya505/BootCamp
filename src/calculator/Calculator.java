75eceafbb7b0f4487445cc3ad2d40b0487467e58
Riya505 <riyapk505@gmail.com>
2022-09-27 00:57:31 +0530

    bugFixed

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -7,7 +7,8 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Calculator");
         int choice;
-        int x, y;
+        int x, y,s;
+        Scanner sc=new Scanner(System.in);
 
         while (true) {
             System.out.println("1. addition");
@@ -15,23 +16,36 @@ public class Calculator {
             System.out.println("3. multiplication");
             System.out.println("4. division");
             System.out.println("5. exit");
-            Scanner sc=new Scanner(System.in);
             choice = sc.nextInt();
-            x=sc.nextInt();
-            y=sc.nextInt();
 
             switch (choice) {
                 case 1:
+                    x=sc.nextInt();
+                    y=sc.nextInt();
                     System.out.println(x + y);
                     break;
                 case 2:
+                    x=sc.nextInt();
+                    y=sc.nextInt();
                     System.out.println(x - y);
                     break;
                 case 3:
+                    x=sc.nextInt();
+                    y=sc.nextInt();
                     System.out.println(x * y);
                     break;
                 case 4:
-                    System.out.println(x / y);
+                    x=sc.nextInt();
+                    y=sc.nextInt();
+                    try {
+                        s = x / y;
+                    }
+                    catch(ArithmeticException e) {
+                        System.out.println("2nd no. zero");
+                        y = sc.nextInt();
+                        s = x / y;
+                    }
+                    System.out.println("Division "+s);
                     break;
                 case 5:
                     System.exit(0);
